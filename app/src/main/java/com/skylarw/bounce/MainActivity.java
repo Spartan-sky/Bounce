c881f3e983b3429f83244fed175338ee2f8e0bc2
Skylar Werner <Skylar_werner@yahoo.com>
2017-10-20 19:15:39 -0500

    Second Commit

--- app/src/main/java/com/skylarw/bounce/MainActivity.java
+++ app/src/main/java/com/skylarw/bounce/MainActivity.java
@@ -1,32 +1,63 @@
 package com.skylarw.bounce;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.Animation;
+import android.view.animation.TranslateAnimation;
 import android.widget.Button;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
     //Variables
-    private Button myButton;
-    private ImageView ball;
-    private int count=0;
+    private Button rngBut, bounBut;
+    private ImageView iconL, iconR;
+
+    //Intent
+    private Intent rngI,bounI;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ball = (ImageView) findViewById(R.id.inGameBall);
-        myButton = (Button) findViewById(R.id.buttonStart);
-        myButton.setOnClickListener( new View.OnClickListener()
+
+        startR();
+        startL();
+
+        rngBut = (Button) findViewById(R.id.buttonStart2);
+        rngBut.setOnClickListener( new View.OnClickListener()
         {
             @Override
-            public void onClick(View view)
+            public void onClick(View v)
             {
-                setContentView(R.layout.in_game);
+                rngI=new Intent(MainActivity.this, BallRng.class);
+                startActivity(rngI);
             }
         });
-
     }
+
+    private void startL()
+    {
+        iconL = (ImageView) findViewById(R.id.iconStart2);
+        final Animation animation = new TranslateAnimation(-400, 0, 0, 0);
+        animation.setDuration(2000);
+        animation.setInterpolator(new AccelerateInterpolator(1.0f));
+        animation.setRepeatCount(-1);
+        iconL.startAnimation(animation);
+    }
+
+    private void startR()
+    {
+        iconR = (ImageView) findViewById(R.id.iconStart1);
+        final Animation animation = new TranslateAnimation(400, 0, 0, 0);
+        animation.setDuration(2000);
+        animation.setInterpolator(new AccelerateInterpolator(1.0f));
+        animation.setRepeatCount(-1);
+        iconR.startAnimation(animation);
+    }
+
 
 }
